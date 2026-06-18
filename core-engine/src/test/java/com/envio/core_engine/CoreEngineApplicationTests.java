c1fd5004897dd5fb4d935921f06159030bae5b4a
chathura2406 <it23689480@my.sliit.lk>
2026-06-18 22:53:48 +0530

    chore: initial project setup with docker and spring boot

---
+++ core-engine/src/test/java/com/envio/core_engine/CoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envio.core_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
